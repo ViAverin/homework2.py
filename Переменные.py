7a0969e01577142075b7a0da3493e28b6f0ee8c4
Victor Averin <victor.cybernetic@gmail.com>
2024-07-14 13:53:03 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+Number_of_completed_tasks = 12
+Number_of_Hours_Spent = 1.5
+Course_name = 'Python'
+Time_for_one_task = Number_of_Hours_Spent / Number_of_completed_tasks
+print('Курс:', Course_name, 'Всего задач:', Number_of_completed_tasks, 'Затрачено часов:', Number_of_Hours_Spent, 'Среднее время выполнения:', Time_for_one_task)
